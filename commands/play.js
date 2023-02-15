32238b735f1de9ea25c3a47705e0eb0d9d187d8d
Pavle Srdic <pavle.srdic@m1.rs>
2023-02-15 20:09:13 +0100

    First Commit

---
+++ commands/play.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { QueryType } = require("discord-player")
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("fizi")
+		.setDescription("play a song from YouTube.")
+        .addStringOption(option =>
+			option.setName("searchterms").setDescription("search keywords").setRequired(true)
+		),
+	execute: async ({ client, interaction }) => {
+        // Make sure the user is inside a voice channel
+		if (!interaction.member.voice.channel) return interaction.reply("You need to be in a Voice Channel to play a song.");
+        
+        // Create a play queue for the server
+		const queue = await client.player.createQueue(interaction.guild);
+
+        // Wait until you are connected to the channel
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+
+		let embed = new EmbedBuilder()
+
+        // Search for the song using the discord-player
+        let url = interaction.options.getString("searchterms")
+        const result = await client.player.search(url, {
+            requestedBy: interaction.user,
+            searchEngine: QueryType.AUTO
+        })
+
+        // finish if no tracks were found
+        if (result.tracks.length === 0)
+            return interaction.editReply("No results")
+        
+        // Add the track to the queue
+        const song = result.tracks[0]
+        await queue.addTrack(song)
+        embed
+            .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+            .setThumbnail(song.thumbnail)
+            .setFooter({ text: `Duration: ${song.duration}`})
+		
+
+        // Play the song
+        if (!queue.playing) await queue.play()
+        
+        // Respond with the embed containing information about the player
+        await interaction.reply({
+            embeds: [embed]
+        })
+	},
+}
